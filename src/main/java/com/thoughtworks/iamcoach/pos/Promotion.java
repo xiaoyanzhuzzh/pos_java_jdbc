824f717482d84c4cb3f46803272ecede2ac7c7a2
zzh <xiaoyanzhuzzh@sina.com>
2014-10-26 13:37:10 +0800

    add new variables

--- src/main/java/com/thoughtworks/iamcoach/pos/Promotion.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/Promotion.java
@@ -1,4 +1,12 @@
 package com.thoughtworks.iamcoach.pos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Promotion {
+    private int promotionId;
+    private String promotionDescribe;
+    private int type;
+
+    private List<Item> itemList = new ArrayList<Item>();  //取决于业务逻辑；
 }
