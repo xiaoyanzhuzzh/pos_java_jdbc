5eaef76437c6f294b0bc277f6642c35b73535208
zzh <xiaoyanzhuzzh@sina.com>
2014-10-26 13:27:30 +0800

    delete useless codes

--- src/test/java/com/thoughtworks/iamcoach/pos/ItemTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/ItemTest.java
@@ -7,7 +7,6 @@ import static org.fest.assertions.api.Assertions.assertThat;
 public class ItemTest {
     @Test
 
-
     public void can_get_arguments_value() {
         Item item = new Item("ITEM000001", "橙汁", "瓶", 5.00);
         assertThat(item.getName()).isEqualTo("橙汁");
@@ -16,14 +15,5 @@ public class ItemTest {
         assertThat(item.getPrice()).isEqualTo(5.00);
         assertThat(item.getPromotion().get(0)).isEqualTo("buy_two_get_one_free_promotion");
     }
-
-
-
-//    public void should_be_mock() {
-//        HelloWorld helloWorld = mock(HelloWorld.class);
-//        when(helloWorld.beenCalled()).thenReturn("Hello World");
-//
-//        assertThat(helloWorld.beenCalled()).isEqualTo("Hello World");
-//    }
 
 }
