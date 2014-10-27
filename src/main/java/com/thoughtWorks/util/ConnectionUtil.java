032e79206b33797defb1d1bb9874d0c2fffb5088
zzh <xiaoyanzhuzzh@sina.com>
2014-10-27 16:32:00 +0800

    add two function of connection

--- src/main/java/com/thoughtWorks/util/ConnectionUtil.java
+++ src/main/java/com/thoughtWorks/util/ConnectionUtil.java
@@ -1,7 +1,34 @@
 package com.thoughtWorks.util;
 
-/**
- * Created by zhangzhihui on 14-10-27.
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionUtil {
+    private String driverName = "com.mysql.jdbc.Driver";
+    private String url = "jdbc:mysql://localhost:3306/management";
+    private String userName = "pos";
+    private String password = "123";
+
+    Connection connection = null;
+
+    public Connection getConnection(){
+        try {
+            Class.forName(driverName);
+            connection = DriverManager.getConnection(url, userName, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public void closeConnection(){
+        try{
+            connection.close();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
