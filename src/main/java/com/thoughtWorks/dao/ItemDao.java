3750660010cfa469c0a0c0172b076299b96bc6b4
zzh <xiaoyanzhuzzh@sina.com>
2014-10-27 16:25:07 +0800

    come true interface methods

---
+++ src/main/java/com/thoughtWorks/dao/ItemDao.java
@@ -0,0 +1,35 @@
+package com.thoughtWorks.dao;
+
+import com.thoughtworks.iamcoach.pos.Item;
+
+import java.util.List;
+
+/**
+ * Created by zhangzhihui on 14-10-27.
+ */
+public class ItemDao implements ItemDaoImpl {
+    @Override
+    public Item getItemById(int id) {
+         
+    }
+
+    @Override
+    public List<Item> getItems() {
+        return null;
+    }
+
+    @Override
+    public void insertItem(Item item) {
+
+    }
+
+    @Override
+    public void deleteItemById(int id) {
+
+    }
+
+    @Override
+    public void updateItem(Item item) {
+
+    }
+}
