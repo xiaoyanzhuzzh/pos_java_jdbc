abc86d44419ec08520bbe0348844d56cd1818bca
zzh <xiaoyanzhuzzh@sina.com>
2014-10-27 16:23:59 +0800

    add methods in interface

--- src/main/java/com/thoughtWorks/dao/ItemDaoImpl.java
+++ src/main/java/com/thoughtWorks/dao/ItemDaoImpl.java
@@ -1,4 +1,13 @@
 package com.thoughtWorks.dao;
 
+import com.thoughtworks.iamcoach.pos.Item;
+
+import java.util.List;
+
 public interface ItemDaoImpl {
+        Item getItemById(int id);
+        List<Item> getItems();
+        void insertItem(Item item);
+        void deleteItemById(int id);
+        void updateItem(Item item);
 }
