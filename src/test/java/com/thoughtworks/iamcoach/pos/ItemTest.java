779b6612f9cc537b8a03bed87fe7379b820ce508
zzh <xiaoyanzhuzzh@sina.com>
2014-10-27 19:46:04 +0800

    test codes

--- src/test/java/com/thoughtworks/iamcoach/pos/ItemTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/ItemTest.java
@@ -8,7 +8,7 @@ public class ItemTest {
     @Test
 
     public void can_get_arguments_value() {
-        Item item = new Item("ITEM000001", "橙汁", "瓶", 5.00);
+        Item item = new Item(null,"ITEM000001", "橙汁", "瓶", 5.00);
         assertThat(item.getName()).isEqualTo("橙汁");
         assertThat(item.getBarcode()).isEqualTo("ITEM000001");
         assertThat(item.getUnit()).isEqualTo("瓶");
