8b69fa59e1b2e80343f5b1442598fc2960490a50
zzh <xiaoyanzhuzzh@sina.com>
2014-10-29 18:48:43 +0800

    add function

--- src/main/java/com/thoughtWorks/vo/SecondHalfMoneyPromotion.java
+++ src/main/java/com/thoughtWorks/vo/SecondHalfMoneyPromotion.java
@@ -1,7 +1,12 @@
 package com.thoughtWorks.vo;
 
-/**
- * Created by zhangzhihui on 14-10-29.
- */
-public class SecondHalfMoneyPromotion {
+import com.thoughtworks.iamcoach.pos.CartItem;
+
+public class SecondHalfMoneyPromotion extends Promotion{
+    @Override
+    public double caculate(CartItem cartItem, double num) {
+        double promotionNumber = ((int) cartItem.getNum())/2;
+        double actualNumber = (cartItem.getNum() - promotionNumber) + promotionNumber/2;
+        return actualNumber * cartItem.getPrice();
+    }
 }
