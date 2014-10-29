fc02aa9de8584b63f94503a5e9e3b259bba8d8e7
zzh <xiaoyanzhuzzh@sina.com>
2014-10-29 18:28:19 +0800

    add getItemByBarcode method

--- src/main/java/com/thoughtWorks/dao/ItemDaoImpl.java
+++ src/main/java/com/thoughtWorks/dao/ItemDaoImpl.java
@@ -1,11 +1,13 @@
 package com.thoughtWorks.dao;
 
-import com.thoughtworks.iamcoach.pos.Item;
+
+import com.thoughtWorks.vo.Item;
 
 import java.util.List;
 
 public interface ItemDaoImpl {
         Item getItemById(int id);
+        Item getItemByBarcode(String barcode);
         List<Item> getItems();
         void insertItem(Item item);
         void deleteItemById(int id);
