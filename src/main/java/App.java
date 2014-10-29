8385fb2b6d90845f048a3b6a36802bbce2d11e42
zzh <xiaoyanzhuzzh@sina.com>
2014-10-29 18:49:25 +0800

    main function change

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,31 +1,38 @@
+import com.thoughtWorks.dao.ItemDao;
+import com.thoughtWorks.dao.ItemDaoImpl;
 import com.thoughtworks.iamcoach.pos.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class App {
 
     public static void main(String[] args){
 
-        ArrayList<Item> items = getItems();
-        Scanner scanner = new Scanner();
-        ArrayList<String> inputs = scanner.getInputs();
-
-        Cart cart = new Cart(inputs, items);
-
-        Printer printer = new Printer(cart.getCartItems());
-        printer.printAllInfo();
+//        ArrayList<Item> items = getItems();
+//        Scanner scanner = new Scanner();
+//        ArrayList<String> inputs = scanner.getInputs();
+//
+//        Cart cart = new Cart(inputs, items);
+//
+//        Printer printer = new Printer(cart.getCartItems());
+//        printer.printAllInfo();
+        List<Item> items = new ArrayList<Item>();
+        ItemDaoImpl itemDao = new ItemDao();
+        System.out.println(itemDao.getItemById(1));
+        System.out.println(itemDao.getItems());
     }
 
-    private static ArrayList<Item> getItems(){
-        ArrayList<Item> items = new ArrayList<Item>();
-
-        items.add(new Item("ITEM000001", "橙汁", "瓶", 5.00));
-        items.add(new Item("ITEM000002", "可乐", "瓶", 3.00));
-        items.add(new Item("ITEM000003", "苹果", "斤", 4.50));
-        items.add(new Item("ITEM000004", "雪碧", "瓶", 5.00));
-        items.add(new Item("ITEM000005", "方便面", "袋", 2.50));
-
-        return items;
-    }
+//    private static ArrayList<Item> getItems(){
+//        ArrayList<Item> items = new ArrayList<Item>();
+//
+//        items.add(new Item(null,"ITEM000001", "橙汁", "瓶", 5.00));
+//        items.add(new Item(null,"ITEM000002", "可乐", "瓶", 3.00));
+//        items.add(new Item(null,"ITEM000003", "苹果", "斤", 4.50));
+//        items.add(new Item(null,"ITEM000004", "雪碧", "瓶", 5.00));
+//        items.add(new Item(null,"ITEM000005", "方便面", "袋", 2.50));
+//
+//        return items;
+//    }
 
 }
