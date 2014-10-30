3c8a1efde34d87fb7b2bffc6250fde5ca1a6e427
zzh <xiaoyanzhuzzh@sina.com>
2014-10-30 22:24:35 +0800

    finish Promotion methods

--- src/main/java/com/thoughtworks/iamcoach/pos/vo/Promotion.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/vo/Promotion.java
@@ -1,7 +1,50 @@
 package com.thoughtworks.iamcoach.pos.vo;
 
-/**
- * Created by zhangzhihui on 14-10-30.
- */
-public class Promotion {
+abstract class Promotion {
+    private String id;
+    private String proDesc;
+    private int type;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProDesc() {
+        return proDesc;
+    }
+
+    public void setProDesc(String proDesc) {
+        this.proDesc = proDesc;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public Promotion(){}
+
+    private Promotion(String id, String proDesc, int type) {
+        this.id = id;
+        this.proDesc = proDesc;
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Promotion{" +
+                "id='" + id + '\'' +
+                ", proDesc='" + proDesc + '\'' +
+                ", type=" + type +
+                '}';
+    }
+
+    public abstract double caculate(Item item, double num);
 }
