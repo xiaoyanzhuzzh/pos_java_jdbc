13bf786272a0c9a2319e1d515d71fed715fca118
zzh <xiaoyanzhuzzh@sina.com>
2014-10-30 23:31:31 +0800

    no test

--- src/test/java/com/thoughtworks/iamcoach/pos/CartTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/CartTest.java
@@ -1,69 +1,69 @@
-package com.thoughtworks.iamcoach.pos;
-
-import org.junit.Test;
-
-import java.util.ArrayList;
-
-import static org.fest.assertions.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-public class CartTest {
-
-    private Item mock_Item(){
-        Item item = mock(Item.class);
-        when(item.getName()).thenReturn("橙汁");
-        when(item.getBarcode()).thenReturn("ITEM000001");
-        when(item.getPrice()).thenReturn(5.00);
-        when(item.getUnit()).thenReturn("瓶");
-
-        return item;
-    }
-
-    private CartItem mock_CartItem(){
-        CartItem cartItem = mock(CartItem.class);
-        when(cartItem.getName()).thenReturn("橙汁");
-        when(cartItem.getBarcode()).thenReturn("ITEM000001");
-        when(cartItem.getPrice()).thenReturn(5.00);
-        when(cartItem.getUnit()).thenReturn("瓶");
-        when(cartItem.getNum()).thenReturn(5.0);
-
-        return cartItem;
-    }
-
-    @Test
-    public void can_get_cartItems(){
-        ArrayList<String> inputs = new ArrayList<String>();
-        inputs.add("ITEM000001-2");
-
-        Item item = this.mock_Item();
-
-        ArrayList<Item> items = new ArrayList<Item>();
-        items.add(item);
-
-        Cart cart = new Cart(inputs, items);
-        assertThat(cart.getCartItems().size()).isEqualTo(1);
-        assertThat(cart.getCartItems().get(0).getNum()).isEqualTo(2);
-    }
-
-    @Test
-    public void can_set_cartItems(){
-        ArrayList<String> inputs = new ArrayList<String>();
-        inputs.add("ITEM000001-2");
-
-        Item item = this.mock_Item();
-        ArrayList<Item> items = new ArrayList<Item>();
-        items.add(item);
-
-        CartItem cartItem = this.mock_CartItem();
-        ArrayList<CartItem> cartItems = new ArrayList<CartItem>();
-        cartItems.add(cartItem);
-
-        Cart cart = new Cart(inputs, items);
-        cart.setCartItems(cartItems);
-
-        assertThat(cart.getCartItems().size()).isEqualTo(1);
-        assertThat(cart.getCartItems().get(0).getNum()).isEqualTo(5.0);
-    }
-
-}
+//package com.thoughtworks.iamcoach.pos;
+//
+//import org.junit.Test;
+//
+//import java.util.ArrayList;
+//
+//import static org.fest.assertions.api.Assertions.assertThat;
+//import static org.mockito.Mockito.mock;
+//import static org.mockito.Mockito.when;
+//
+//public class CartTest {
+//
+//    private Item mock_Item(){
+//        Item item = mock(Item.class);
+//        when(item.getName()).thenReturn("橙汁");
+//        when(item.getBarcode()).thenReturn("ITEM000001");
+//        when(item.getPrice()).thenReturn(5.00);
+//        when(item.getUnit()).thenReturn("瓶");
+//
+//        return item;
+//    }
+//
+//    private CartItem mock_CartItem(){
+//        CartItem cartItem = mock(CartItem.class);
+//        when(cartItem.getName()).thenReturn("橙汁");
+//        when(cartItem.getBarcode()).thenReturn("ITEM000001");
+//        when(cartItem.getPrice()).thenReturn(5.00);
+//        when(cartItem.getUnit()).thenReturn("瓶");
+//        when(cartItem.getNum()).thenReturn(5.0);
+//
+//        return cartItem;
+//    }
+//
+//    @Test
+//    public void can_get_cartItems(){
+//        ArrayList<String> inputs = new ArrayList<String>();
+//        inputs.add("ITEM000001-2");
+//
+//        Item item = this.mock_Item();
+//
+//        ArrayList<Item> items = new ArrayList<Item>();
+//        items.add(item);
+//
+//        Cart cart = new Cart(inputs, items);
+//        assertThat(cart.getCartItems().size()).isEqualTo(1);
+//        assertThat(cart.getCartItems().get(0).getNum()).isEqualTo(2);
+//    }
+//
+//    @Test
+//    public void can_set_cartItems(){
+//        ArrayList<String> inputs = new ArrayList<String>();
+//        inputs.add("ITEM000001-2");
+//
+//        Item item = this.mock_Item();
+//        ArrayList<Item> items = new ArrayList<Item>();
+//        items.add(item);
+//
+//        CartItem cartItem = this.mock_CartItem();
+//        ArrayList<CartItem> cartItems = new ArrayList<CartItem>();
+//        cartItems.add(cartItem);
+//
+//        Cart cart = new Cart(inputs, items);
+//        cart.setCartItems(cartItems);
+//
+//        assertThat(cart.getCartItems().size()).isEqualTo(1);
+//        assertThat(cart.getCartItems().get(0).getNum()).isEqualTo(5.0);
+//    }
+//
+//}
--- src/test/java/com/thoughtworks/iamcoach/pos/ItemTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/ItemTest.java
@@ -5,15 +5,15 @@ import org.junit.Test;
 import static org.fest.assertions.api.Assertions.assertThat;
 
 public class ItemTest {
-    @Test
-
-    public void can_get_arguments_value() {
-        Item item = new Item(null,"ITEM000001", "橙汁", "瓶", 5.00);
-        assertThat(item.getName()).isEqualTo("橙汁");
-        assertThat(item.getBarcode()).isEqualTo("ITEM000001");
-        assertThat(item.getUnit()).isEqualTo("瓶");
-        assertThat(item.getPrice()).isEqualTo(5.00);
-        assertThat(item.getPromotion().get(0)).isEqualTo("buy_two_get_one_free_promotion");
-    }
+//    @Test
+//
+//    public void can_get_arguments_value() {
+//        Item item = new Item(null,"ITEM000001", "橙汁", "瓶", 5.00);
+//        assertThat(item.getName()).isEqualTo("橙汁");
+//        assertThat(item.getBarcode()).isEqualTo("ITEM000001");
+//        assertThat(item.getUnit()).isEqualTo("瓶");
+//        assertThat(item.getPrice()).isEqualTo(5.00);
+//        assertThat(item.getPromotion().get(0)).isEqualTo("buy_two_get_one_free_promotion");
+//    }
 
 }
