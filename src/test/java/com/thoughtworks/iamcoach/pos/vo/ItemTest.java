fe3d76912cb187c70a78764949ac04ad723a065c
zzh <xiaoyanzhuzzh@sina.com>
2014-10-31 19:57:24 +0800

    finish Item test

--- src/test/java/com/thoughtworks/iamcoach/pos/vo/ItemTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/vo/ItemTest.java
@@ -1,4 +1,19 @@
 package com.thoughtworks.iamcoach.pos.vo;
 
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class ItemTest {
+        @Test
+
+    public void can_get_arguments_value() {
+        Item item = new Item(null,"ITEM000001", "橙汁", "瓶", 5.00);
+        assertThat(item.getName()).isEqualTo("橙汁");
+        assertThat(item.getBarcode()).isEqualTo("ITEM000001");
+        assertThat(item.getUnit()).isEqualTo("瓶");
+        assertThat(item.getPrice()).isEqualTo(5.00);
+        assertThat(item.getPromotionList().size()).isEqualTo(0);
+    }
+
 }
