3bcede3cbacb95d18c343c23507f442cfd51ca31
zzh <xiaoyanzhuzzh@sina.com>
2014-10-31 20:21:52 +0800

    finish category test

--- src/test/java/com/thoughtworks/iamcoach/pos/vo/CategoryTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/vo/CategoryTest.java
@@ -1,7 +1,16 @@
 package com.thoughtworks.iamcoach.pos.vo;
 
-/**
- * Created by zhangzhihui on 14-10-31.
- */
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class CategoryTest {
+    @Test
+
+    public void can_get_arguments_value() {
+        Category category = new Category(null, "水果");
+        assertThat(category.getName()).isEqualTo("橙汁");
+        assertThat(category.getId()).isEqualTo(null);
+    }
+
 }
