0e2a89bd4ca38cbc4fbafc78a063f51f06fdf5df
zzh <xiaoyanzhuzzh@sina.com>
2014-10-31 22:22:26 +0800

    CartItem extends Item

--- src/main/java/com/thoughtworks/iamcoach/pos/service/CartItem.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/CartItem.java
@@ -1,7 +1,10 @@
 package com.thoughtworks.iamcoach.pos.service;
 
+import com.thoughtworks.iamcoach.pos.vo.Item;
+
 /**
  * Created by zhangzhihui on 14-10-31.
  */
-public class CartItem {
+public class CartItem extends Item{
+    
 }
