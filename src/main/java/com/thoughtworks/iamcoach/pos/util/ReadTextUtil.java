2a946e4d16e0613181327c28722ce31b56179b30
zzh <xiaoyanzhuzzh@sina.com>
2014-10-31 22:02:00 +0800

    finish getText method

--- src/main/java/com/thoughtworks/iamcoach/pos/util/ReadTextUtil.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/util/ReadTextUtil.java
@@ -1,7 +1,19 @@
 package com.thoughtworks.iamcoach.pos.util;
 
-/**
- * Created by zhangzhihui on 14-10-31.
- */
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.*;
+
 public class ReadTextUtil {
+
+    public List<String> getText(String fileName) {
+        List<String> text = new ArrayList<String>();
+        try {
+            Path path = FileSystems.getDefault().getPath("src/main/resources/", fileName);
+            text =  Files.readAllLines(path);
+        } catch (IOException ex) {
+            System.out.println("fail read file!");
+        }
+        return text;
+    }
 }
