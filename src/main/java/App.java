80246ac8e034dcfd5a7d92d8601aec480a541c27
zzh <xiaoyanzhuzzh@sina.com>
2014-10-31 21:43:16 +0800

    modify App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
-import com.thoughtWorks.dao.ItemDao;
-import com.thoughtWorks.dao.ItemDaoImpl;
-import com.thoughtworks.iamcoach.pos.*;
+import com.thoughtworks.iamcoach.pos.dao.ItemDao;
+import com.thoughtworks.iamcoach.pos.dao.ItemDaoImpl;
+import com.thoughtworks.iamcoach.pos.vo.Item;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,30 +9,11 @@ public class App {
 
     public static void main(String[] args){
 
-//        ArrayList<Item> items = getItems();
-//        Scanner scanner = new Scanner();
-//        ArrayList<String> inputs = scanner.getInputs();
-//
-//        Cart cart = new Cart(inputs, items);
-//
-//        Printer printer = new Printer(cart.getCartItems());
-//        printer.printAllInfo();
         List<Item> items = new ArrayList<Item>();
         ItemDaoImpl itemDao = new ItemDao();
-        System.out.println(itemDao.getItemById(1));
         System.out.println(itemDao.getItems());
+        System.out.println(itemDao.getItemByBarcode("ITEM000001"));
+        System.out.println(itemDao.getItemByBarcode("ITEM000001").getPromotionList());
     }
-
-//    private static ArrayList<Item> getItems(){
-//        ArrayList<Item> items = new ArrayList<Item>();
-//
-//        items.add(new Item(null,"ITEM000001", "橙汁", "瓶", 5.00));
-//        items.add(new Item(null,"ITEM000002", "可乐", "瓶", 3.00));
-//        items.add(new Item(null,"ITEM000003", "苹果", "斤", 4.50));
-//        items.add(new Item(null,"ITEM000004", "雪碧", "瓶", 5.00));
-//        items.add(new Item(null,"ITEM000005", "方便面", "袋", 2.50));
-//
-//        return items;
-//    }
 
 }
