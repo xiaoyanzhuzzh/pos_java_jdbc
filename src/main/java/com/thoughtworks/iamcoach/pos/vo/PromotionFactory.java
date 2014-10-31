99ddf8dca0b0a2bc40b760eb2c7444fff782dc0f
zzh <xiaoyanzhuzzh@sina.com>
2014-10-31 20:41:07 +0800

    finish getPromotionByType methods

--- src/main/java/com/thoughtworks/iamcoach/pos/vo/PromotionFactory.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/vo/PromotionFactory.java
@@ -1,7 +1,23 @@
 package com.thoughtworks.iamcoach.pos.vo;
 
-/**
- * Created by zhangzhihui on 14-10-31.
- */
 public class PromotionFactory {
+    public static final int DISCOUNT = 1;
+    public static final int SECOND_HALF_MONEY = 2;
+    public static final int BUY_TWO_GET_ONE = 3;
+
+    public static Promotion getPromotionByType(int type) {
+        Promotion promotion = null;
+        switch (type) {
+            case BUY_TWO_GET_ONE:
+                promotion = new BuyTwoGetOnePromotion();
+                break;
+            case SECOND_HALF_MONEY:
+                promotion = new SecondHalfMoneyPromotion();
+                break;
+            case DISCOUNT:
+                promotion = new DiscountPromotion();
+                break;
+        }
+        return promotion;
+    }
 }
