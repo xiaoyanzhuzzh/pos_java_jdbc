c46dfd0090f4f7fcd4a0e554392b48f0bf4f515c
zzh <xiaoyanzhuzzh@sina.com>
2014-11-01 22:54:03 +0800

    App methods

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,10 @@
+import com.thoughtworks.iamcoach.pos.dao.CategoryDaoImple;
 import com.thoughtworks.iamcoach.pos.dao.CategoryDao;
-import com.thoughtworks.iamcoach.pos.dao.CategoryDaoImpl;
-import com.thoughtworks.iamcoach.pos.dao.ItemDao;
-import com.thoughtworks.iamcoach.pos.dao.ItemDaoImpl;
+import com.thoughtworks.iamcoach.pos.service.Cart;
+import com.thoughtworks.iamcoach.pos.service.ItemServiceImple;
 import com.thoughtworks.iamcoach.pos.service.ItemService;
-import com.thoughtworks.iamcoach.pos.service.ItemServiceImpl;
+import com.thoughtworks.iamcoach.pos.service.Scanner;
+import com.thoughtworks.iamcoach.pos.vo.CartItem;
 import com.thoughtworks.iamcoach.pos.vo.Item;
 
 import java.util.ArrayList;
@@ -12,14 +13,11 @@ import java.util.List;
 public class App {
 
     public static void main(String[] args){
-
-        List<Item> items = new ArrayList<Item>();
-        ItemServiceImpl itemService = new ItemService();
-        System.out.println(itemService.getItems());
-        System.out.println(itemService.getItemByBarcode("ITEM000001"));
-
-        CategoryDaoImpl categoryDao = new CategoryDao();
-        System.out.println(categoryDao.getCategoryById("1"));
+        ItemService itemServiceImple = new ItemServiceImple();
+        Scanner scanner = new Scanner();
+        Cart cart = new Cart(scanner, itemServiceImple);
+        Printer printer = new Printer(cart);
+        printer.printAllInfo();
     }
 
 }
