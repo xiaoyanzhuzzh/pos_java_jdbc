650533d42abee9bacc1ce9033dfc6476c37fbe2b
zzh <xiaoyanzhuzzh@sina.com>
2014-11-01 21:42:54 +0800

    finish can_get_nubers_test method

--- src/test/java/com/thoughtworks/iamcoach/pos/service/CartTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/service/CartTest.java
@@ -1,7 +1,23 @@
 package com.thoughtworks.iamcoach.pos.service;
 
-/**
- * Created by zhangzhihui on 14-11-1.
- */
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class CartTest {
+    @Test
+    public void can_get_CartItems_test(){
+        Cart cart = new Cart();
+        assertThat(cart.getCartItems().size()).isEqualTo(0);
+    }
+
+    @Test
+    public void can_get_numbers_test(){
+        Cart cart = new Cart();
+        List<String> input = new ArrayList<String>();
+        assertThat(cart.getNumber(input).size()).isEqualTo(0);
+    }
 }
