2d7230b44571ecb34dbeb82f940c1839d2187309
zzh <xiaoyanzhuzzh@sina.com>
2014-11-01 09:42:30 +0800

    implement methods

--- src/main/java/com/thoughtworks/iamcoach/pos/service/PromotionService.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/PromotionService.java
@@ -1,4 +1,17 @@
 package com.thoughtworks.iamcoach.pos.service;
 
-public class PromotionService {
+import com.thoughtworks.iamcoach.pos.vo.Promotion;
+
+import java.util.List;
+
+public class PromotionService implements PromotionServiceImpl {
+    @Override
+    public Promotion getPromotionById(String id) {
+        return null;
+    }
+
+    @Override
+    public List<Promotion> getPromotions() {
+        return null;
+    }
 }
--- src/main/java/com/thoughtworks/iamcoach/pos/service/PromotionServiceImpl.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/PromotionServiceImpl.java
@@ -1,4 +1,10 @@
 package com.thoughtworks.iamcoach.pos.service;
 
+import com.thoughtworks.iamcoach.pos.vo.Promotion;
+
+import java.util.List;
+
 public interface PromotionServiceImpl {
+    Promotion getPromotionById(String id);
+    List<Promotion> getPromotions();
 }
