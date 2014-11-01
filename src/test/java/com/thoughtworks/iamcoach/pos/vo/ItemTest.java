b79318776d7427df5e4a4a736b92ca1e3b42ef06
zzh <xiaoyanzhuzzh@sina.com>
2014-11-01 09:08:19 +0800

    add categoryId parament

--- src/test/java/com/thoughtworks/iamcoach/pos/vo/ItemTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/vo/ItemTest.java
@@ -5,10 +5,10 @@ import org.junit.Test;
 import static org.fest.assertions.api.Assertions.assertThat;
 
 public class ItemTest {
-        @Test
+    @Test
 
     public void can_get_arguments_value() {
-        Item item = new Item(null,"ITEM000001", "橙汁", "瓶", 5.00);
+        Item item = new Item(null,"ITEM000001", "橙汁", "瓶", 5.00, "饮品");
         assertThat(item.getName()).isEqualTo("橙汁");
         assertThat(item.getBarcode()).isEqualTo("ITEM000001");
         assertThat(item.getUnit()).isEqualTo("瓶");
