14f413789c0f258c931b0f2cae85b0602eb396ef
zzh <xiaoyanzhuzzh@sina.com>
2014-11-01 22:40:43 +0800

    correct CartService test

--- src/test/java/com/thoughtworks/iamcoach/pos/service/CartTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/service/CartTest.java
@@ -1,23 +1,26 @@
 package com.thoughtworks.iamcoach.pos.service;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 public class CartTest {
-    @Test
-    public void can_get_CartItems_test(){
-        Cart cart = new Cart();
-        assertThat(cart.getCartItems().size()).isEqualTo(0);
+    Scanner scanner;
+    ItemService itemServiceImple;
+    @Before
+    public void before(){
+        scanner = mock(Scanner.class);
+        itemServiceImple = mock(ItemServiceImple.class);
     }
 
     @Test
-    public void can_get_numbers_test(){
-        Cart cart = new Cart();
-        List<String> input = new ArrayList<String>();
-        assertThat(cart.getNumber(input).size()).isEqualTo(0);
+    public void can_get_CartItems_test(){
+        Scanner scanner = new Scanner();
+        ItemService itemServiceImple = new ItemServiceImple();
+        Cart cart = new Cart(scanner, itemServiceImple);
+        assertThat(cart.getCartItems().size()).isEqualTo(3);
     }
+
 }
