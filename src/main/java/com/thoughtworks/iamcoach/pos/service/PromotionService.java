2d6d9f0fac9feb70084d396b062b85382086c408
zzh <xiaoyanzhuzzh@sina.com>
2014-11-01 10:23:08 +0800

    finish two methods in PromotionService

--- src/main/java/com/thoughtworks/iamcoach/pos/service/PromotionService.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/PromotionService.java
@@ -1,17 +1,20 @@
 package com.thoughtworks.iamcoach.pos.service;
 
+import com.thoughtworks.iamcoach.pos.dao.PromotionDao;
+import com.thoughtworks.iamcoach.pos.dao.PromotionDaoImpl;
 import com.thoughtworks.iamcoach.pos.vo.Promotion;
 
 import java.util.List;
 
 public class PromotionService implements PromotionServiceImpl {
+    PromotionDaoImpl promotionDao = new PromotionDao();
     @Override
     public Promotion getPromotionById(String id) {
-        return null;
+        return promotionDao.getPromotionById(id);
     }
 
     @Override
     public List<Promotion> getPromotions() {
-        return null;
+        return promotionDao.getPromotions();
     }
 }
