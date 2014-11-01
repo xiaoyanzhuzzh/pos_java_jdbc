abbf47b40bebff3ad7314d55248e8a947ab647bd
zzh <xiaoyanzhuzzh@sina.com>
2014-11-01 13:01:34 +0800

    finish can_get_values_test method

--- src/test/java/com/thoughtworks/iamcoach/pos/vo/CartItemTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/vo/CartItemTest.java
@@ -1,7 +1,19 @@
 package com.thoughtworks.iamcoach.pos.vo;
 
-/**
- * Created by zhangzhihui on 14-11-1.
- */
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class CartItemTest {
+    @Test
+    public void can_get_values_test(){
+        Item item = new Item(null, "ITEM000002", "橙汁", "瓶", 5.0, "2");
+        double number = 5.0;
+        CartItem cartItem = new CartItem(item, number);
+        assertThat(cartItem.getItem().getBarcode()).isEqualTo("ITEM000002");
+        assertThat(cartItem.getItem().getUnit()).isEqualTo("瓶");
+        assertThat(cartItem.getItem().getName()).isEqualTo("chengz");
+        assertThat(cartItem.getNumber()).isEqualTo(5.0);
+    }
+
 }
