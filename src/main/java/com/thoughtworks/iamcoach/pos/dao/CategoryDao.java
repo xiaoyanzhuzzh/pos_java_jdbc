763cb3f11522de0f7b9312e90401fa0555c4baff
zzh <xiaoyanzhuzzh@sina.com>
2014-11-01 08:59:41 +0800

    finish getCategoryById methods

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/CategoryDao.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/CategoryDao.java
@@ -1,13 +1,39 @@
 package com.thoughtworks.iamcoach.pos.dao;
 
+import com.thoughtworks.iamcoach.pos.util.ConnectionUtil;
 import com.thoughtworks.iamcoach.pos.vo.Category;
+import com.thoughtworks.iamcoach.pos.vo.Item;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class CategoryDao implements CategoryDaoImpl{
+    ConnectionUtil connectionUtil = new ConnectionUtil();
     @Override
     public Category getCategoryById(String id) {
-        return null;
+        Category category = null;
+        String sql = "SELECT * FROM categories WHERE id = '"+id+"'";
+        Connection connection = connectionUtil.getConnection();
+        Statement statement = null;
+        ResultSet rs = null;
+
+        try {
+            statement = connection.createStatement();
+            rs = statement.executeQuery(sql);
+            rs.next();
+
+            category = new Category(rs.getString("id"), rs.getString("name"));
+
+            rs.close();
+            statement.close();
+            connectionUtil.closeConnection();
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return category;
     }
 
     @Override
