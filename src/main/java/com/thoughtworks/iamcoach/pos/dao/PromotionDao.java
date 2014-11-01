6e6315ba89ea3718e2fbcca5f5b75effa772ecdc
zzh <xiaoyanzhuzzh@sina.com>
2014-11-01 10:37:17 +0800

    rename

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/PromotionDao.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/PromotionDao.java
@@ -1,83 +1,13 @@
 package com.thoughtworks.iamcoach.pos.dao;
 
-import com.thoughtworks.iamcoach.pos.util.ConnectionUtil;
-import com.thoughtworks.iamcoach.pos.vo.*;
+import com.thoughtworks.iamcoach.pos.vo.Promotion;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
-import static com.thoughtworks.iamcoach.pos.vo.PromotionFactory.getPromotionByType;
-
-public class PromotionDao implements PromotionDaoImpl{
-    ConnectionUtil connectionUtil = new ConnectionUtil();
-
-    @Override
-    public Promotion getPromotionById(String id) {
-        Promotion promotion = null;
-        String sql = "SELECT * FROM promotions WHERE id = '"+id+"'";
-        Connection connection = connectionUtil.getConnection();
-        Statement statement = null;
-        ResultSet rs = null;
-
-        try {
-            statement = connection.createStatement();
-            rs = statement.executeQuery(sql);
-            rs.next();
-
-            promotion = getPromotionByType(rs.getInt("type"));
-            promotion.setId(rs.getString("id"));
-            promotion.setProDesc(rs.getString("proDesc"));
-            promotion.setType(rs.getInt("type"));
-
-            rs.close();
-            statement.close();
-            connectionUtil.closeConnection();
-        }catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return promotion;
-    }
-
-    @Override
-    public List<Promotion> getPromotions() {
-        List<Promotion> promotions = new ArrayList<Promotion>();
-        Promotion promotion = null;
-
-        String sql = "SELECT * FROM promotions";
-        Connection connection = connectionUtil.getConnection();
-        Statement statement = null;
-        ResultSet rs = null;
-        try {
-            statement = connection.createStatement();
-            rs = statement.executeQuery(sql);
-            while(rs.next()){
-
-                promotion = getPromotionByType(rs.getInt("type"));
-
-                promotion.setId(rs.getString("id"));
-                promotion.setProDesc(rs.getString("proDesc"));
-                promotion.setType(rs.getInt("type"));
-                promotions.add(promotion);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return promotions;
-    }
-
-    @Override
-    public void insertPromotion(Promotion promotion) {
-
-    }
-
-    @Override
-    public void deletePromotionById(String id) {
-
-    }
-
-    @Override
-    public void updatePromtion(Promotion promotion) {
-
-    }
+public interface PromotionDao {
+    Promotion getPromotionById(String id);
+    List<Promotion> getPromotions();
+    void insertPromotion(Promotion promotion);
+    void deletePromotionById(String id);
+    void updatePromtion(Promotion promotion);
 }
