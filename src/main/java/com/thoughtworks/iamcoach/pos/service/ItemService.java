73dc575f720e8f303f26883ec085cc150d8558f6
zzh <xiaoyanzhuzzh@sina.com>
2014-11-01 09:28:49 +0800

    implements methods

--- src/main/java/com/thoughtworks/iamcoach/pos/service/ItemService.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/ItemService.java
@@ -1,7 +1,21 @@
 package com.thoughtworks.iamcoach.pos.service;
 
-/**
- * Created by zhangzhihui on 14-11-1.
- */
-public class ItemService {
+import com.thoughtworks.iamcoach.pos.dao.ItemDao;
+import com.thoughtworks.iamcoach.pos.dao.ItemDaoImpl;
+import com.thoughtworks.iamcoach.pos.vo.Item;
+
+import java.util.List;
+
+public class ItemService implements ItemServiceImpl{
+    ItemDaoImpl itemDao = new ItemDao();
+
+    @Override
+    public Item getItemByBarcode(String barcode) {
+        return itemDao.getItemByBarcode(barcode);
+    }
+
+    @Override
+    public List<Item> getItems() {
+        return itemDao.getItems();
+    }
 }
