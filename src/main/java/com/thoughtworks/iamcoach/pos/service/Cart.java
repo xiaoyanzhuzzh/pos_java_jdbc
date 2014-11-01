8b888d5bc03d2715df9234f7ffbdb7833e8829b9
zzh <xiaoyanzhuzzh@sina.com>
2014-11-01 22:16:01 +0800

    add getCartItems method

--- src/main/java/com/thoughtworks/iamcoach/pos/service/Cart.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/Cart.java
@@ -1,7 +1,85 @@
 package com.thoughtworks.iamcoach.pos.service;
 
-/**
- * Created by zhangzhihui on 14-11-1.
- */
+import com.thoughtworks.iamcoach.pos.vo.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
+    private List<String> inputs;
+    private List<Item> items;
+
+    public Cart(Scanner scanner, ItemService itemServiceImple){
+        this.inputs = scanner.getInputs();
+        this.items = itemServiceImple.getItems();
+    }
+
+    public List<CartItem> getCartItems() {
+        ArrayList<CartItem> cartItems = new ArrayList<CartItem>();
+        ArrayList<Promotion> promotionList = null;
+
+        double[] numbers = getNumbers();
+
+        for(int i = 0; i < numbers.length; i++){
+            if(numbers[i] != 0){
+                cartItems.add(new CartItem(items.get(i), numbers[i]));
+            }
+        }
+        return cartItems;
+    }
+
+    public double getTotalSum() {
+        List<CartItem> cartItems = this.getCartItems();
+
+        double total = 0;
+        for(CartItem aCartItem: cartItems){
+            double actualSubTotal = aCartItem.getNumber() * aCartItem.getItem().getPrice();
+            total += actualSubTotal;
+        }
+
+        return total;
+    }
+
+    public double getActualSum() {
+        List<CartItem> cartItems = this.getCartItems();
+
+        double actutalTotal = 0;
+        for(CartItem aCartItem: cartItems){
+            actutalTotal += aCartItem.getSubtotal();
+        }
+        return actutalTotal;
+    }
+
+    private double[] getNumbers() {
+        double numbers[] = new double[items.size()];
+
+        for(int i=0; i<items.size(); i++){
+            for(int j=0; j<inputs.size(); j++){
+                boolean canSplit = inputs.get(j).contains("-");
+                if(canSplit){
+                    numbers[i] +=  processSplitedBarcode(i, j);
+                }else{
+                    numbers[i] += processBarcode(i, j);
+                }
+            }
+        }
+        return numbers;
+    }
+
+    private double processSplitedBarcode(int i, int j){
+        double number = 0;
+
+        String[] barcodeAndNumber = inputs.get(j).split("-");
+        if(items.get(i).getBarcode().equals(barcodeAndNumber[0])){
+            number = Double.parseDouble(barcodeAndNumber[1]);
+        }
+        return number;
+    }
+
+    private double processBarcode(int i, int j){
+        if(items.get(i).getBarcode().equals(inputs.get(j))){
+            return 1.0;
+        }
+        return 0.0;
+    }
 }
