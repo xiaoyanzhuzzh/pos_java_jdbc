2abea26d2ef570a91d74b000e9e87db9099b3cfa
zzh <xiaoyanzhuzzh@sina.com>
2014-11-01 21:29:37 +0800

    finish can_get_ppromotion_test

--- src/test/java/com/thoughtworks/iamcoach/pos/vo/PromotionFactoryTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/vo/PromotionFactoryTest.java
@@ -1,7 +1,27 @@
 package com.thoughtworks.iamcoach.pos.vo;
 
-/**
- * Created by zhangzhihui on 14-11-1.
- */
+import org.junit.Test;
+
+import static com.thoughtworks.iamcoach.pos.vo.PromotionFactory.getPromotionByType;
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class PromotionFactoryTest {
+
+    @Test
+    public void can_get_promotion_of_discount_promotion(){
+        Promotion promotion = getPromotionByType(3);
+        assertThat(promotion instanceof DiscountPromotion).isTrue();
+    }
+
+    @Test
+    public void can_get_promotion_of_second_half_money(){
+        Promotion promotion = getPromotionByType(2);
+        assertThat(promotion instanceof SecondHalfMoneyPromotion).isTrue();
+    }
+
+    @Test
+    public void can_get_promotion_of_buy_two_get_one(){
+        Promotion promotion = getPromotionByType(1);
+        assertThat(promotion instanceof BuyTwoGetOnePromotion).isTrue();
+    }
 }
