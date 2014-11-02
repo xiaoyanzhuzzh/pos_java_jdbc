03c9d241c5cd64703522515c19f18cdfccb957dc
zzh <xiaoyanzhuzzh@sina.com>
2014-11-02 11:16:15 +0800

    modify main function

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,6 +13,7 @@ import java.util.List;
 public class App {
 
     public static void main(String[] args){
+
         ItemService itemServiceImple = new ItemServiceImple();
         Scanner scanner = new Scanner();
         Cart cart = new Cart(scanner, itemServiceImple);
