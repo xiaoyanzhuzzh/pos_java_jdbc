413943a24e4631be54972010180db924fe5c7d73
zzh <xiaoyanzhuzzh@sina.com>
2014-11-03 23:00:46 +0800

    implement methods

--- src/main/java/com/thoughtworks/iamcoach/pos/service/CategoryServiceImpl.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/CategoryServiceImpl.java
@@ -1,7 +1,21 @@
 package com.thoughtworks.iamcoach.pos.service;
 
-/**
- * Created by zhangzhihui on 14-11-3.
- */
-public class CategoryServiceImpl {
+import com.thoughtworks.iamcoach.pos.dao.CategoryDao;
+import com.thoughtworks.iamcoach.pos.dao.CategoryDaoImple;
+import com.thoughtworks.iamcoach.pos.vo.Category;
+
+import java.util.List;
+
+public class CategoryServiceImpl implements CategoryService{
+    private CategoryDao categoryDaoImple = new CategoryDaoImple();
+
+    @Override
+    public Category getCategoryById(String id) {
+        return categoryDaoImple.getCategoryById(id);
+    }
+
+    @Override
+    public List<Category> getCategories() {
+        return categoryDaoImple.getCategories();
+    }
 }
