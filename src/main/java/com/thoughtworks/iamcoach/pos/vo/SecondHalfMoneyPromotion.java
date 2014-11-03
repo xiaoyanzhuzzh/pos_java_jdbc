a11d95f340bb3b11e6d127c9fb901e19b615f82e
zzh <xiaoyanzhuzzh@sina.com>
2014-11-03 21:35:24 +0800

    modify children method

--- src/main/java/com/thoughtworks/iamcoach/pos/vo/SecondHalfMoneyPromotion.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/vo/SecondHalfMoneyPromotion.java
@@ -2,9 +2,9 @@ package com.thoughtworks.iamcoach.pos.vo;
 
 public class SecondHalfMoneyPromotion extends Promotion{
     @Override
-    public double calculate(Item item, double number) {
-        double promotionNumber = ((int) number)/2;
-        double actualNumber = (number - promotionNumber) + promotionNumber/2;
-        return actualNumber * item.getPrice();
+    public double calculate(CartItem cartItem, double discount) {
+        double promotionNumber = ((int) cartItem.getNumber())/2;
+        double actualNumber = (cartItem.getNumber() - promotionNumber) + promotionNumber/2;
+        return actualNumber * cartItem.getItem().getPrice();
     }
 }
