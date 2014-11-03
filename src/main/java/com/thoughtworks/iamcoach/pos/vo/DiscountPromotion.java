6b0b76a654987f2e3fd71e9e249f8c398c843fbe
zzh <xiaoyanzhuzzh@sina.com>
2014-11-03 21:35:01 +0800

    modify children method

--- src/main/java/com/thoughtworks/iamcoach/pos/vo/DiscountPromotion.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/vo/DiscountPromotion.java
@@ -1,38 +1,8 @@
 package com.thoughtworks.iamcoach.pos.vo;
 
-import java.io.IOException;
-import java.nio.file.*;
-import java.util.ArrayList;
-
 public class DiscountPromotion extends Promotion{
     @Override
-    public double calculate(Item item, double number) {
-        double discount = getDiscount(item);
-        return number * discount * item.getPrice();
-    }
-
-    private double getDiscount(Item item){
-        double discount = 0;
-        ArrayList<String> discountPromotions = getDiscountPromotion();
-
-        for (String aDiscountPromotion: discountPromotions){
-            String[] currentPromotion = aDiscountPromotion.split(":");
-            if(item.getBarcode().equals(currentPromotion[0])){
-                discount = Double.parseDouble(currentPromotion[1])/100;
-            }
-        }
-        return discount;
-    }
-
-    private ArrayList<String> getDiscountPromotion(){
-
-        ArrayList<String> promotionText = new ArrayList<String>();
-        try {
-            Path path = FileSystems.getDefault().getPath("src/main/resources/", "discount_promotion.txt");
-            promotionText = (ArrayList<String>) Files.readAllLines(path);
-        } catch (IOException ex) {
-            System.out.println("fail read file!");
-        }
-        return promotionText;
+    public double calculate(CartItem cartItem, double discount) {
+        return cartItem.getNumber() * discount * cartItem.getItem().getPrice();
     }
 }
