380a68c2b1dafeced8a68f1ed125e8075d0df1e9
zzh <xiaoyanzhuzzh@sina.com>
2014-11-03 23:10:51 +0800

    about main function

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,5 @@
-import com.thoughtworks.iamcoach.pos.dao.CategoryDaoImple;
-import com.thoughtworks.iamcoach.pos.dao.CategoryDao;
-import com.thoughtworks.iamcoach.pos.service.Cart;
-import com.thoughtworks.iamcoach.pos.service.ItemServiceImple;
-import com.thoughtworks.iamcoach.pos.service.ItemService;
-import com.thoughtworks.iamcoach.pos.service.Scanner;
-import com.thoughtworks.iamcoach.pos.vo.CartItem;
-import com.thoughtworks.iamcoach.pos.vo.Item;
+import com.thoughtworks.iamcoach.pos.service.*;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class App {
 
