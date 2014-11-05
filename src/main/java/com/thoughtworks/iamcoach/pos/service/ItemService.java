0c1eb657b419a05b4b12f0aab081ef91981a448f
zzh <xiaoyanzhuzzh@sina.com>
2014-11-05 18:22:24 +0800

    add getPromotionsByItemId method

--- src/main/java/com/thoughtworks/iamcoach/pos/service/ItemService.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/ItemService.java
@@ -1,10 +1,12 @@
 package com.thoughtworks.iamcoach.pos.service;
 
 import com.thoughtworks.iamcoach.pos.vo.Item;
+import com.thoughtworks.iamcoach.pos.vo.Promotion;
 
 import java.util.List;
 
 public interface ItemService {
     Item getItemByBarcode(String barcode);
     List<Item> getItems();
+    List<Promotion> getPromotionsByItemId(String id);
 }
