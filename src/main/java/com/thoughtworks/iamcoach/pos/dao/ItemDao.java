270e707dc2a59f5635378d963eb7f1fd27fd0d7a
zzh <xiaoyanzhuzzh@sina.com>
2014-11-05 17:56:23 +0800

    add getPromotionsByItemId function

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/ItemDao.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/ItemDao.java
@@ -1,10 +1,12 @@
 package com.thoughtworks.iamcoach.pos.dao;
 
 import com.thoughtworks.iamcoach.pos.vo.Item;
+import com.thoughtworks.iamcoach.pos.vo.Promotion;
 
 import java.util.List;
 
 public interface ItemDao {
         Item getItemByBarcode(String barcode);
         List<Item> getItems();
+        List<Promotion> getPromotionsByItemId(String id);
 }
