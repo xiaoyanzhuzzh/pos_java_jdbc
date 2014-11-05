7416f7b555881cf258b8f3a52fc435dd47f4bd26
zzh <xiaoyanzhuzzh@sina.com>
2014-11-05 18:34:49 +0800

    refactor functions

--- src/main/java/com/thoughtworks/iamcoach/pos/service/Cart.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/Cart.java
@@ -6,42 +6,75 @@ import java.util.*;
 
 public class Cart {
     private List<String> inputs;
-    private List<Item> items = new ArrayList<Item>();
+    private ItemService itemServiceImple;
+//    private List<Item> items = new ArrayList<Item>();
 
     public Cart(Scanner scanner, ItemService itemServiceImple){
         this.inputs = scanner.getInputs();
-        this.items = itemServiceImple.getItems();
+        this.itemServiceImple = itemServiceImple;
+//        this.items = itemServiceImple.getItems();
     }
 
     public List<CartItem> getCartItems() {
-        ArrayList<CartItem> cartItems = new ArrayList<CartItem>();
-        double[] numbers = getNumbers();
+        List<CartItem> cartItems = new ArrayList<CartItem>();
 
-        for(int i = 0; i < numbers.length; i++){
+        List<Item> items = itemServiceImple.getItems();
+
+        double[] numbers = getNumbers(items);
+
+        for(int i=0; i<numbers.length; i++){
             if(numbers[i] != 0){
+                items.get(i).setPromotionList(itemServiceImple.getPromotionsByItemId((items.get(i).getId())));
                 cartItems.add(new CartItem(items.get(i), numbers[i]));
             }
         }
+        System.out.println(cartItems);
         return cartItems;
     }
+//        ArrayList<CartItem> cartItems = new ArrayList<CartItem>();
+//        double[] numbers = getNumbers();
+//
+//        for(int i = 0; i < numbers.length; i++){
+//            if(numbers[i] != 0){
+//                cartItems.add(new CartItem(items.get(i), numbers[i]));
+//            }
+//        }
+//        return cartItems;
+//    }
 
-    private double[] getNumbers() {
+    private double[] getNumbers(List<Item> items) {
         double numbers[] = new double[items.size()];
 
-        for(int i = 0; i < items.size(); i++){
-            for(int j = 0; j < inputs.size(); j++){
+        for(int i=0; i<items.size(); i++){
+            for(int j=0; j<inputs.size(); j++){
                 boolean canSplit = inputs.get(j).contains("-");
                 if(canSplit){
-                    numbers[i] +=  SplitBarcodeAndGetNumber(i, j);
+                    numbers[i] +=  splitBarcodeAndGetNumber(i, j, items);
                 }else{
-                    numbers[i] += getNumber(i, j);
+                    numbers[i] += getNumber(i, j, items);
                 }
             }
         }
         return numbers;
     }
 
-    private double SplitBarcodeAndGetNumber(int i, int j){
+//    private double[] getNumbers() {
+//        double numbers[] = new double[items.size()];
+//
+//        for(int i = 0; i < items.size(); i++){
+//            for(int j = 0; j < inputs.size(); j++){
+//                boolean canSplit = inputs.get(j).contains("-");
+//                if(canSplit){
+//                    numbers[i] +=  SplitBarcodeAndGetNumber(i, j);
+//                }else{
+//                    numbers[i] += getNumber(i, j);
+//                }
+//            }
+//        }
+//        return numbers;
+//    }
+
+    private double splitBarcodeAndGetNumber(int i, int j, List<Item> items){
         double number = 0;
 
         String[] barcodeAndNumber = inputs.get(j).split("-");
@@ -51,7 +84,7 @@ public class Cart {
         return number;
     }
 
-    private double getNumber(int i, int j){
+    private double getNumber(int i, int j, List<Item> items){
         if(items.get(i).getBarcode().equals(inputs.get(j))){
             return 1.0;
         }
