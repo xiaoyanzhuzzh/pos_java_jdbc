d9064463c4184b33997b43bcf8780540a73c9f0a
zzh <xiaoyanzhuzzh@sina.com>
2014-11-05 18:36:03 +0800

    delete useless codes

--- src/main/java/com/thoughtworks/iamcoach/pos/vo/CartItem.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/vo/CartItem.java
@@ -1,8 +1,6 @@
 package com.thoughtworks.iamcoach.pos.vo;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class CartItem {
     private Item item;
